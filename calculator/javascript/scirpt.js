cb8e0d3675bb6da239c966d5c25a7ab99fb47262
sxpphickat <sphhhsh@gmail.com>
2023-11-06 11:07:34 -0300

    add: new numbers and operators

--- calculator/javascript/scirpt.js
+++ calculator/javascript/scirpt.js
@@ -36,21 +36,59 @@ function operate(numA, numB, operator) {
 }
 
 
-const result = operate(123, 0, "/");
+/* const result = operate(123, 0, "/");
 
-console.log(result);
+console.log(result); */
 
 let leftOperand = 0; // vou multiplicar por 10 e dps somar o numero digitado
 let rightOperand = 0;
+let operator = "";
 let bufferNum = 0;
+let operationResult = 0;
 
 const buttons = document.querySelector(".buttons");
+
+const result = document.querySelector(".result p");
+const equation = document.querySelector(".equation p");
 
 buttons.addEventListener("click", (event) => {
 	const target = event.target;
 	if (target.classList.contains("number")) {
+		/* equation.innerText = operationResult; */
 		bufferNum *= 10;
 		bufferNum += parseInt(target.id);
+		result.innerText = bufferNum;
 	}
-	console.log({bufferNum})
+	if (target.classList.contains("operator")) {
+		leftOperand = bufferNum;
+		equation.innerText += ` ${leftOperand} `;
+		equation.innerText += ` ${target.innerText} `;
+		bufferNum = 0;
+		operator = target.id;
+	}
+	if (target.classList.contains("eq")) {
+		console.log("here");
+		switch (operator) {
+			case "times":
+				operationResult = multiply(leftOperand, bufferNum);
+				break ;
+			case "add":
+				operationResult = add(leftOperand, bufferNum);
+				break ;
+			case "sub":
+				operationResult = subtract(leftOperand, bufferNum);
+				break ;
+			case "div":
+				operationResult = divide(leftOperand, bufferNum);
+				break ;
+		}
+		equation.innerText += ` ${bufferNum} =`;
+		result.innerText = operationResult;
+		bufferNum = 0;
+	}
+	
+	console.log({bufferNum});
+	console.log({leftOperand});
+	//console.log({rightOperand});
+	console.log({operator});
 });
